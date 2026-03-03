dd6a96d6dbdaca88b45063ee5977700731bc448a
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2026-03-03 09:10:08 +0600

    Refactor function abcd to demonstrate variable scope with var

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ var a;
 var a = 12;
 let a = 12;
 const a = 12;
+
+function abcd() {
+  if (true) {
+    var a = 12;
+  }
+  // Accessing 'a' from outside of if...else block
+  console.log(a); // 12
+}
