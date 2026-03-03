0f099d16754a30c0c323859d7eb6252a319b3ba1
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2026-03-03 09:50:29 +0600

    Hoisting example

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// words vs keywords
-// var, let, const - line-by-line comparison
+console.log(c);
 
-a = 12; // bad ❌
-var a;
-var a = 12;
-let a = 12;
-const a = 12;
-
-function abcd() {
-  if (true) {
-    var a = 12;
-  }
-  // Accessing 'a' from outside of if...else block
-  console.log(a); // 12
-}
+let c = 12;
